1b26536f440846f1e75f6531a029e72cdc698fb6
Andersson <aonavas@scl.edu.gt>
2022-09-29 21:14:00 -0600

    buscador finalizado en archivo js y css

--- js/componentes/menu.js
+++ js/componentes/menu.js
@@ -2,5 +2,7 @@ export let menu = `
 <header class="barragris">
     <img class="logo" src="img/Recurso 4 1.png">
     <h2 class="titulo">Medical Techno Graphics</h2>
+    <img class="lupa" src="img/Lupa.png">
 </header>
+<div class="busca"> <input  class="buscador" type="text" placeholder="Encuentra un Alimento"> </div>
 `;
