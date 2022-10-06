e83f1b8de7e74b2c10c41a5cf41b61a8d7b4054c
davidnatareno <ednatareno@scl.edu.gt>
2022-10-06 02:21:38 -0600

    Caja de busqueda

---
+++ js/componentes/caja_buscador.js
@@ -0,0 +1,13 @@
+function abrir(){
+    var ventana = document.getElementById("ventana")
+    
+    ventana.classList.remove("ocultar")
+    
+    }
+    
+    function cerrar(){
+    var equis = document.getElementById("cierre")
+    
+    equis.classList.add("ocultar")
+    
+    }
--- js/componentes/menu.js
+++ js/componentes/menu.js
@@ -2,7 +2,12 @@ export let menu = `
 <header class="barragris">
     <img class="logo" src="img/Logo.png">
     <h2 class="titulo">Medical Techno Graphics</h2>
-    <img class="lupa" src="img/Lupa.png">
+    <img class="lupa" id="abrir" src="img/Lupa.png">
 </header>
-<div class="busca"> <input  class="buscador" type="text" placeholder="Encuentra un Alimento"> </div>
+ <div class="cuadro_ocultar">
+    <img class="exit" id="cerrar"src="https://github.com/ervindavid12/IMg/blob/main/marca-x.png?raw=true">
+    <div class="busca"> 
+    <input  class="buscador" type="text" placeholder="Encuentra un Alimento">
+    </div>
+ </div>
 `;
