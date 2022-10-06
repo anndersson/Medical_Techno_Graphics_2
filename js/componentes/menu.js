0b3c3e309a2d78ab05242f9e228760e8d965428f
davidnatareno <ednatareno@scl.edu.gt>
2022-10-06 07:41:30 -0600

    Arreglo de proteinas y lipidos

--- js/componentes/menu.js
+++ js/componentes/menu.js
@@ -2,17 +2,17 @@ export let menu = `
 <header class="barragris">
     <img class="logo" src="img/Logo.png">
     <h2 class="titulo">Medical Techno Graphics</h2>
-<<<<<<< HEAD
+
     <img class="lupa" id="abrir" src="img/Lupa.png">
 </header>
  <div class="cuadro_ocultar">
-    <img class="exit" id="cerrar"src="https://github.com/ervindavid12/IMg/blob/main/marca-x.png?raw=true">
+    
     <div class="busca"> 
     <input  class="buscador" type="text" placeholder="Encuentra un Alimento">
     </div>
  </div>
-=======
+
 </header>
  <div class="busca"> <input  class="buscador" type="text" placeholder= &#128269;"Encuentra un Alimento"> </div>
->>>>>>> 391c0145ea104d693671560655502e371cadd31d
+
 `;
